61e859278cd6a0336ad4edea3cc2f5f17c63a878
killerkeemstar <gguess1337@gmail.com>
2022-03-27 19:04:00 +0200

    first commit

---
+++ gen_words.py
@@ -0,0 +1,11 @@
+def load_words_len_x(x) -> set:
+    with open("words_alpha.txt", "r") as words:
+        return list(filter(None, [len(i) == x and i for i in words.read().split()]))
+
+
+if __name__ == "__main__":
+    x = 28
+    # print(load_words_len_x(x))
+    print(len(load_words_len_x(x)))
+    with open("test.txt", "w") as f:
+        f.write("".join(str(w) + "\n" for w in load_words_len_x(x)))
---
+++ main.py
@@ -0,0 +1,106 @@
+from gen_words import load_words_len_x
+from termcolor import colored, cprint
+import sys
+import os
+import random
+
+
+def clear():
+    os.system("cls")
+
+
+def get_word(words) -> str:
+    return words[random.randint(0, len(words) - 1)]
+
+
+def gen_new_board(lenght, guesses):
+    return [[None for j in range(lenght)] for i in range(guesses)]
+
+
+def draw_board(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            print("".join("[-]" if board[i][j] == None else board[i][j]), end="")
+        print()
+
+
+def draw_score(score, maxscore, word):
+    print("".join(str(score + 1) + "/" + str(maxscore)))
+    print(word)
+
+
+def count_letters(word):
+    l_count = [0] * 26
+    for c in word:
+        l_count[ord(c) - 97] += 1
+    return l_count
+
+
+def new_guess(board, words, word, score):
+    guess = ""
+    while len(guess) <= 1:
+        # while not guess in words:
+        print("".join("#" for i in range(len(board[0]))), end="\r")
+        guess = str(input())
+    # guess = [i for i in guess]
+    word_letters = count_letters(word)
+
+    for i in range(len(guess)):
+        if word_letters[ord(guess[i]) - 97] == 0:
+            board[score][i] = colored("[" + guess[i] + "]", "white")
+        elif guess[i] == word[i]:
+            board[score][i] = colored("[" + guess[i] + "]", "green")
+            word_letters[ord(guess[i]) - 97] -= 1
+        else:
+            if word_letters[ord(guess[i]) - 97] > 0 and board[score][i] == None:
+                board[score][i] = colored("[" + guess[i] + "]", "yellow")
+                word_letters[ord(guess[i]) - 97] -= 1
+
+    if word == guess:
+        return True
+    else:
+        return False
+
+        # if word[i] == guess[i]:
+        #     board[score][i] = colored("[" + guess[i] + "]", "green")
+        # elif guess[i] not in word:
+        #     board[score][i] = colored("[" + guess[i] + "]", "white")
+        # else:
+
+
+# board = [
+#     [None, None, None, None, None],
+#     [None, None, None, None, None],
+#     [None, None, None, None, None],
+#     [None, None, None, None, None],
+#     [None, None, None, None, None],
+#     [None, None, None, None, None],
+# ]
+
+if __name__ == "__main__":
+    clear()
+    win = False
+    x = int(input("Word's length: "))
+    y = int(input("How many guesses: "))
+    score = 0
+    words = load_words_len_x(x)
+    word = get_word(words)
+    # words.append("abble")
+    # words.append("pable")
+    # word = "apple"
+    board = gen_new_board(x, y)
+    while win == False and score <= y:
+        clear()
+        draw_score(score, y, word)
+        draw_board(board)
+        win = new_guess(board, words, word, score)
+        score += 1
+    clear()
+    if win == True:
+        print("Won")
+    else:
+        print("Lost")
+
+    # while not win:
+    #     draw_board(board)
+    #     new_guess(board, words, word, score)
